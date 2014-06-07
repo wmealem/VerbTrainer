78c072907a2700778a56ba55cb087c3937d02421
Michael Ealem <michael.ealem@gmail.com>
2014-06-07 09:20:11 -0700

    added support for normal screen output, cloze deletion format, and cloze deletion import format

--- languages/spanish.py
+++ languages/spanish.py
@@ -9,6 +9,8 @@ _PRONOUNS = SpanishCategory('yo', 'tú', 'vos', 'él/ella/usted',
                            'ellos/ellas/ustedes')
 
 _STD_FORMAT = '{} {}'
+_STD_CLOZE_FORMAT = '{0} {{{{c1::{1}::{2}, {3}}}}}'
+
 # Endings for the simple tenses
 _ENDINGS =\
     {'ar':
@@ -67,3 +69,53 @@ def construct_inflection(infinitive, tense):
     stem_and_ending = construct_stem_and_ending(infinitive, tense)
     return SpanishCategory._make([item for item in zip(_PRONOUNS,
                                                        stem_and_ending)])
+def output_normal_view(infinitive, tense, conj):
+    '''
+    Pretty-printing for the traditional two-column output
+    of a verb conjugation
+    '''
+    return ['{}, {}:'.format(infinitive, tense),
+    ('⎯'*45), '{:<25}‖ {}'.format(_STD_FORMAT.format(*conj.fps),
+                                  _STD_FORMAT.format(*conj.fpp)),
+    '{:<25}‖ {}'.format(_STD_FORMAT.format(*conj.sps),
+                        _STD_FORMAT.format(*conj.spp)),
+    '{:<25}‖'.format(_STD_FORMAT.format(*conj.spsv)),
+    '{:<25}‖ {}'.format(_STD_FORMAT.format(*conj.tps),
+                        _STD_FORMAT. format(*conj.tpp))]
+
+def output_cloze(infinitive, tense, conj):
+    '''
+    Combines the different parts of a verb conjugation with
+    Anki's required formatting to produce a form suitable
+    for a cloze-deletion card
+    '''
+    result = []
+    # TODO - make this pythonic, it's an ugly hack as it is
+    for i, item in enumerate(conj):
+       result.append(_STD_CLOZE_FORMAT.format(item[0], item[1],
+                                             infinitive, tense))
+    return SpanishCategory._make(result)
+
+
+def output_cloze_import(infinitive, tense, translation, sound, conj):
+    '''
+    Combines the output of the output_cloze function with optional
+    translation and sound fields and combines them to produce the
+    format required for Anki's import function
+    '''
+    cloze = output_cloze(infinitive, tense, conj)
+    if translation:
+        add_trn = [cz + ('|{}'.format(trn)) for cz, trn in
+                   zip(cloze, translation)]
+    else:
+        add_trn = [cz + '|' for cz in cloze]
+
+    if sound:
+        add_snd = [trn + ('|[sound:{}]'.format(snd)) for
+                   trn, snd in zip(add_trn, sound)]
+    else:
+        add_snd = [trn + '|' for trn in add_trn]
+
+    add_tag = [snd + ('|{}'.format(infinitive)) for snd in add_snd]
+
+    return SpanishCategory._make(add_tag)
